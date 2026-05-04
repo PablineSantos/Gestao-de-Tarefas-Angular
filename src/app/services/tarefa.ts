4ca407217e8760e970034f8307a66de97b89a3d0
PablineSantos <pablines2006@gmail.com>
2026-05-04 15:14:30 -0300

    Service

--- src/app/services/tarefa.ts
+++ src/app/services/tarefa.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Tarefa} from '../models/tarefa/tarefa.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Tarefa {}
+export class TarefaService {
+
+  private tarefas: Tarefa[] = [];
+
+  listar(): Tarefa[] {
+    return this.tarefas;
+  }
+
+  criar(tarefa: Tarefa) {
+    tarefa.id = Date.now();
+    tarefa.dataCriacao = new Date();
+    this.tarefas.push(tarefa);
+  }
+
+  deletar(id: number) {
+    this.tarefas = this.tarefas.filter(t => t.id !== id);
+  }
+
+  toggleConclusao(tarefa: Tarefa) {
+    tarefa.concluida = !tarefa.concluida;
+    tarefa.dataConclusao = tarefa.concluida ? new Date() : undefined;
+  }
+}
